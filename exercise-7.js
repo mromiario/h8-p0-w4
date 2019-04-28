e2c6f309b8e7316b11f5efd7f2c57e7ef7b58f49
mromiario <mromiario@gmail.com>
2019-04-28 21:15:09 +0700

    Exercise 7-Urutkan abjad

---
+++ exercise-7.js
@@ -0,0 +1,13 @@
+//Exercise 7 - M Romi Ario
+function urutkanAbjad(str) {
+   var newArr = str.split('').sort();
+  return newArr.join('');
+
+}
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
