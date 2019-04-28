4f045131a793023abaef7ae39ca9cb5e831055c8
mromiario <mromiario@gmail.com>
2019-04-28 17:51:47 +0700

    Exercise 4 - FPB

---
+++ exercise-2.js
@@ -0,0 +1,19 @@
+//Exercise 2 - M Romi Ario - Week 4
+function fpb(angka1, angka2) {
+    // Membagi angka sampai sisanya 0
+    var modulo;
+    while(angka2 !== 0){
+        modulo = angka1%angka2;
+        angka1 = angka2;
+        angka2 = modulo;
+
+  }
+    return angka1;
+}
+
+// TEST CASES
+console.log(fpb(12, 16)); // 4
+console.log(fpb(50, 40)); // 10
+console.log(fpb(22, 99)); // 11
+console.log(fpb(24, 36)); // 12
+console.log(fpb(17, 23)); // 1
